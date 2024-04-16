5487f849d487a6676775e83199f52af54e5141d1
iremserenay <irem@serenay.net.tr>
2024-04-16 09:27:55 +0300

    animating colors and working with keyframes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,7 +25,7 @@ export default function Header() {
         <motion.button
           onClick={handleStartAddNewChallenge}
           className="button"
-          whileHover={{ scale: 1.1 }}
+          whileHover={{ scale: 1.1, backgroundColor: "#8b11f0" }}
           transition={{ type: "spring", stiffness: 500 }}
         >
           Add Challenge
